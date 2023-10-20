151cc0f5f7f95485f80d9f84470be6c52b42a292
littllenightmare <kulkovangel05@yandex.com>
2023-10-20 09:22:13 +0300

    Обновила библиотеку, добавив функции для матриц

--- LibMas/Class1.cs
+++ LibMas/Class1.cs
@@ -31,11 +31,10 @@
             }
             return sum;
         }
-        /// <summary>
-        /// Функция для записи массива в файл
-        /// </summary>
-        /// <param name="mas">сам массив</param>
-        /// <param name="filename">имя файла</param>
+/// <summary>
+/// Функция для записи массива в файл
+/// </summary>
+/// <param name="mas">массив</param>
         public static void SaveMas(int[] mas)
         {
             StreamWriter sw = new StreamWriter("Массив.txt");
@@ -47,10 +46,9 @@
             sw.Close();
         }
         /// <summary>
-        /// Функция для чтения массива из файла
+        /// ФУнкция для открытия массива из файла
         /// </summary>
-        /// <param name="mas">сам массив</param>
-        /// <param name="filename">имя файла</param>
+        /// <param name="mas">массив</param>
         public static void OpenMas(out int[] mas)
         {
             StreamReader sr = new StreamReader("Массив.txt");
@@ -59,6 +57,45 @@
             for (int i = 0; i < len; i++)
             {
                 mas[i] = Convert.ToInt32(sr.ReadLine());
+            }
+            sr.Close();
+        }
+        /// <summary>
+        /// Функция для записи матрицы в массив
+        /// </summary>
+        /// <param name="matr">матрица</param>
+        public static void SaveMatr(int[,]matr)
+        {
+            StreamWriter sw = new StreamWriter("Матрица.txt");
+            int rows = matr.GetLength(0);
+            int columns = matr.GetLength (1);
+            sw.WriteLine($"{rows}{columns}");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    sw.Write(matr[i,j] + " ");
+                }
+                sw.WriteLine();
+            }
+            sw.Close();
+        }
+        /// <summary>
+        /// Функция для записи матрицы из файла
+        /// </summary>
+        /// <param name="matr">матрица</param>
+        public static void OpenMatr(out int[,] matr) 
+        {
+            StreamReader sr = new StreamReader("Матрица.txt");
+            int rows = Convert.ToInt32(sr.ReadLine());
+            int columns = Convert.ToInt32(sr.ReadLine());
+            matr = new int[rows,columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matr[i,j] = Convert.ToInt32(sr.Read());
+                }
             }
             sr.Close();
         }
